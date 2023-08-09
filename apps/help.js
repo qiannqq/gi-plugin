3814dc289d860c2d9fb085d184cc940d59888315
千奈千祁 <2632139786@qq.com>
2023-08-09 10:28:28 +0800

    增加帮助图片渲染

--- apps/help.js
+++ apps/help.js
@@ -1,4 +1,11 @@
-import plugin from "../../../lib/plugins/plugin";
+import plugin from "../../../lib/plugins/plugin.js";
+import puppeteer from "../../../lib/puppeteer/puppeteer.js"
+//import help from "../resources/html/help.html"
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const Text = 
 `===【Gi插件使用帮助】===
 扔漂流瓶：扔出一个漂流瓶，每天只能扔一次
@@ -7,7 +14,7 @@ const Text =
 互动更新：互动插件更新
 互动强制更新：忽略本地冲突，强制更新`
 
-export class help extends plugin {
+export class example2 extends plugin {
     constructor(){
         super({
             name: 'Gi互动:帮助',
@@ -23,7 +30,21 @@ export class help extends plugin {
         })
     }
     async help(e){
-        e.reply(Text)
+        //e.reply(Text)
+        //e.reply(__dirname)
+        image(e, 'help', 'help', {
+
+        });
         return true;
     }
+}
+async function image(e, file, name) {
+    let data = {
+        quality: 100,
+        tplFile: `./plugins/Gi-plugin/resources/html/help.html`,
+    }
+    let img = puppeteer.screenshot(name, {
+        ...data,
+      })
+      e.reply(img)
 }
