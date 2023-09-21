f6b41e401677d10240c5f1aaa8782c69067d318f
千奈千祁 <2632139786@qq.com>
2023-09-21 13:58:04 +0800

    扔漂流瓶时移除漂流瓶中可能包含的@字符

--- apps/漂流瓶.js
+++ apps/漂流瓶.js
@@ -61,6 +61,7 @@ export class plp extends plugin {
         if(message.type == 'image'){
             plp_ = message.url
         }
+        plp_ = plp_.replace(/@/g, '');
         plp = `@${plp_}；${e.user_id}`
         fs.appendFile(filePath, plp + '\n', 'utf8', (err) => {
             if (err) {
