06374a2121a44cb63bfae8c36687a51bd9ca3e38
千奈千祁 <2632139786@qq.com>
2023-11-11 15:02:39 +0800

    优化帮助图

--- apps/help.js
+++ apps/help.js
@@ -1,6 +1,7 @@
 import plugin from "../../../lib/plugins/plugin.js";
 import image from "../model/image.js";
 import getconfig from "../model/cfg.js";
+import cfg from '../../../lib/config/config.js'
 
 export class example2 extends plugin {
     constructor(){
@@ -11,7 +12,7 @@ export class example2 extends plugin {
             priority: 500,
             rule:[
                 {
-                    reg: '#?(Gi|互动)帮助',
+                    reg: '^#?(Gi|互动|gI|GI|gi)(帮助|菜单|help|功能|说明|指令|使用说明|命令)$',
                     fnc: 'help'
                 }
             ]
@@ -20,11 +21,23 @@ export class example2 extends plugin {
     async help(e){
        let _path = process.cwd().replace(/\\/g, '/')
        const { config } = getconfig(`defSet`, `help`)
+       let botname = cfg.package.name
+       if (cfg.package.name == `yunzai`) {
+        botname = `Yunzai-Bot`
+       } else if (cfg.package.name == `miao-yunzai`){
+        botname = `Miao-Yunzai`
+       } else if (cfg.package.name == `trss-yunzai`){
+        botname = `TRSS-Yunzai`
+       } else if (cfg.package.name == `a-yunzai`){
+        botname = `A-Yunzai`
+       }
        let { img } = await image(e, 'help', 'help', {
             saveId: 'help',
             cwd: _path,
             genshinPath: `${_path}/plugins/genshin/resources/`,
             helpData: config,
+            MiaoV: cfg.package.version,
+            botname: botname
        })
        e.reply(img)
     }
