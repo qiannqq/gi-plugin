edc23daa7be78e10afd22d264b7a7fb995849ee4
千奈千祁 <2632139786@qq.com>
2024-06-26 01:19:00 +0800

    chore: 减少多余的代码部分

--- apps/查分.js
+++ apps/查分.js
@@ -23,40 +23,36 @@ export class chafen extends plugin {
         data.push({ title: '考生号', value: e.user_id })
         if(!fs.existsSync(`${GiPath}/data/gaokao`)) await fs.promises.mkdir(`${GiPath}/data/gaokao`)
         let total_score = 0
+        let subject
         if(fs.existsSync(`${GiPath}/data/gaokao/2024_${e.user_id}.json`)) {
             try {
-                let subject = JSON.parse(await fs.promises.readFile(`${GiPath}/data/gaokao/2024_${e.user_id}.json`, 'utf-8'))
-                let total_score = 0
-                for (let item of subject) {
-                    total_score = total_score + item.value
-                }
-                data.push(...subject, { title: '总分', value: total_score })
-                let { img } = await image(e, 'chafen', 'chafen', {
-                    data,
-                    vCode: await Gimodel.getRandom64Code()
-                })
-                await e.reply([segment.at(e.user_id), `\n考生号：${e.user_id}\n(每年仅能查一次)\n你的成绩为：`, img])
+                subject = JSON.parse(await fs.promises.readFile(`${GiPath}/data/gaokao/2024_${e.user_id}.json`, 'utf-8'))
             } catch (error) {
-                
+                subject = []
             }
         } else {
-            let subject = [
+            subject = [
                 { title: '语文', value: await Gimodel.getReadmeNumber(111, 40) },
                 { title: '数学、物理', value: await Gimodel.getReadmeNumber(182, 70) },
                 { title: '英语', value: await Gimodel.getReadmeNumber(111, 40) },
                 { title: '生物、化学', value: await Gimodel.getReadmeNumber(142, 60) },
             ]
-            for (let item of subject) {
-                total_score = total_score + item.value
-            }
-            data.push(...subject, { title: '总分', value: total_score })
-            let { img } = await image(e, 'chafen', 'chafen', {
-                data,
-                vCode: await Gimodel.getRandom64Code()
-            })
-            await fs.promises.writeFile(`${GiPath}/data/gaokao/2024_${e.user_id}.json`, JSON.stringify(subject, null, 3), 'utf-8')
-            await e.reply([segment.at(e.user_id), `\n考生号：${e.user_id}\n(每年仅能查一次)\n你的成绩为：`, img])
         }
+        if(subject.length != 4) {
+            await fs.promises.unlink(`${GiPath}/data/gaokao/2024_${e.user_id}.json`)
+            await e.reply('查询失败，请稍后重试')
+            return false
+        }
+        for (let item of subject) {
+            total_score = total_score + item.value
+        }
+        data.push(...subject, { title: '总分', value: total_score })
+        let { img } = await image(e, 'chafen', 'chafen', {
+            data,
+            vCode: await Gimodel.getRandom64Code()
+        })
+        await fs.promises.writeFile(`${GiPath}/data/gaokao/2024_${e.user_id}.json`, JSON.stringify(subject, null, 3), 'utf-8')
+        await e.reply([segment.at(e.user_id), `\n考生号：${e.user_id}\n(每年仅能查一次)\n你的成绩为：`, img])
         return true;
     }
 }
